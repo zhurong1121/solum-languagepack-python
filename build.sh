1e1a0f7d638ef915cee3d91141e96b8b9be2d080
Murali Allada <murali.allada@rackspace.com>
2015-01-14 13:50:08 -0600

    Implementing lamguagepack contract
    - Added build.sh. This script installs python dependencies from
      a solum app's requirements.txt file.

---
+++ build.sh
@@ -0,0 +1,13 @@
+# Check command line arguments
+if [ $# -lt 1 ]; then
+  TLOG Usage: $0 req_file
+  exit 1
+fi
+
+REQFILE=$1
+shift
+
+# TODO: check if pip is installed
+
+# install dependencies from app requirements file
+pip install $REQFILE 
