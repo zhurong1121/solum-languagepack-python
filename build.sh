03d787818f9864ab948984977840f81fdacf1839
Melissa Kam <melissa.kam@rackspace.com>
2015-06-02 13:29:17 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-
-# TODO: check if pip is installed
+# Check if pip is installed
 pip help
 [[ $? != 0 ]] && echo python-pip is not installed. && exit 1
 
+# Install dependencies from app requirements file
 cd /app
-
-# install dependencies from app requirements file
 pip install -r requirements.txt
-
-exit 0
